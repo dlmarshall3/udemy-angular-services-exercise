fe71931ef18a3043014683d5e4bc8a8aa70ee09a
David Marshall <dlmarshall3@protonmail.com>
2022-10-19 15:54:25 -0500

    finished product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { WikipediaService } from './wikipedia.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'WikiSearch';
+  pages = [];
+
+  constructor(private wikipediaService: WikipediaService){}
+
+  onTerm(term: string){
+    this.wikipediaService.search(term).subscribe((response: any) => {
+      this.pages = response.query.search;
+    })
+  }
+
 }
